0ae698121793bbf46df077eb807ab914f125fb2c
mnabeel-canva <mnabeel@canva.com>
2023-12-02 00:17:46 +1100

    wippuzzle2

---
+++ 2023/day1/test.py
@@ -0,0 +1,27 @@
+
+
+list_of_numbers_as_dict = {
+    "one": 1,
+    "two": 2,
+    "three": 3,
+    "four": 4,
+    "five": 5,
+    "six": 6,
+    "seven": 7,
+    "eight": 8,
+    "nine": 9,
+}
+
+
+word = "eightwothree"
+updated_word = ""
+results = []
+
+# for word_number in list(list_of_numbers_as_dict.keys()):
+#     if word_number in word:
+#         print(word_number)
+#         word = word.replace(word_number, str(list_of_numbers_as_dict[word_number]))
+#         results.append(list_of_numbers_as_dict[word_number])
+#         print(list_of_numbers_as_dict[word_number])
+
+print(word[])
